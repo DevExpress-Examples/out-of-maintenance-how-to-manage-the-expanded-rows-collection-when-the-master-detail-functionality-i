7779003307a78693fe9acc446f60115f408c94b8
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-11 13:26:33 +0400

    Change example status to out of maintenance

--- VB/Models/Product.vb
+++
@@ -1,7 +0,0 @@
-﻿Namespace DxExample
-    Public Class Product
-        Public Property ProductName() As String
-        Public Property UnitPrice() As Decimal
-        Public Property CategoryID() As Integer
-    End Class
-End Namespace
